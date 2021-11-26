deea8b9e11e1ebe67c51d52fd4122f6ec393826e
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2021-11-26 17:04:24 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { StyleSheet, Text, TextInput, View, Button, Image } from "react-native";
 import { useState } from "react";
 import Cat from "./Cat";
-import house from "./assets/house.png";
-import bed from "./assets/bed.png";
-import kitchen from "./assets/kitchen.png";
-import living_room from "./assets/living_room.png";
 
 export default function App() {
-
   const [boxColor1, setBoxColor1] = useState("red");
   const [boxColor2, setBoxColor2] = useState("red");
   const [boxColor3, setBoxColor3] = useState("red");
@@ -28,66 +23,112 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={{ width: 60, height: 60 }}
+          source={require("./assets/house.png")}
+        />
 
-      <View style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
-      <img style={{ width: 60, height: 60 }} src={house} alt="house" />
-      <h3>SmartHome</h3>
+        <Text style={styles.text}>SmartHome</Text>
       </View>
 
-      <h1>Rooms</h1>
+      <Text style={styles.text}>Rooms</Text>
 
-      <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignContent: "center", alignItems: "center"}}>
-      <img style={{ width: 130, height: 150, background: '#C5D6FF', margin: 7 }} src={bed}/>
-      <img style={{ width: 130, height: 150, background: '#C5D6FF', margin: 7 }} src={kitchen}/>
-      <img style={{ width: 130, height: 150, background: '#C5D6FF', margin: 7 }} src={living_room}/>
-      </View>
+      <View style={styles.rooms}>
+        <View style={styles.roomsContainer}>
+          <View style={styles.roomsbackground}>
+            <Image
+              style={styles.image}
+              source={require("./assets/living_room.png")}
+            />
+            <Text>Living Room</Text>
+          </View>
 
-      <h1>Devices</h1>
+          <View style={styles.roomsbackground}>
+            <Image style={styles.image} source={require("./assets/bed.png")} />
+            <Text>Bedroom</Text>
+          </View>
 
-    <View style = {{width: "100%"}}>
-      <View style = {{position: "absolute", display: "flex", flexDirection: "column"}}>  
-      
-        <View style = {{margin: "10px", width: 425,height: 100,backgroundColor: '#F4FD89'}} />
-
-        {/* <View style = {{margin: "10px",width: 425,height: 100,backgroundColor: '#F4FD89'}} />*/}
-
-        {/* <View style = {{margin: "10px",width: 425,height: 100,backgroundColor: '#F4FD89'}} />*/}
-
-        <View style = {{position: "absolute"}}>
-        <h3>Living Room Lamp</h3>
-        <h3>TV</h3>
-        <h3>Heater</h3>
+          <View style={styles.roomsbackground}>
+            <Image
+              style={styles.image}
+              source={require("./assets/kitchen.png")}
+            />
+            <Text>Kitchen</Text>
+          </View>
         </View>
-
       </View>
-    </View>
 
+      <Text style={styles.text}>Devices</Text>
 
-    <View style = {{width: "80%", paddingLeft: "400px", top: "10px"}}>
-    <View style = {{width: "3%", position: "absolute"}}>
+      <View style={{ width: "100%" }}>
+        <View style={styles.devices}>
+          <View style={styles.yellowBox}>
+            <View style={styles.livingRoom}>
+              <View style={styles.box}></View>
+              <Text>Living Room Lamp</Text>
+            </View>
 
-    <Button title="On" onPress={() => setColorFunction1("green")}></Button>
-    <Button title="Off" onPress={() => setColorFunction1("red")}></Button>
+            <View>
+              <Button
+                title="On"
+                onPress={() => setColorFunction1("green")}
+              ></Button>
+              <Button
+                title="Off"
+                onPress={() => setColorFunction1("red")}
+              ></Button>
+            </View>
+          </View>
+        </View>
+      </View>
 
-    <Button title="On" onPress={() => setColorFunction2("green")}></Button>
-    <Button title="Off" onPress={() => setColorFunction2("red")}></Button>
+      <View style={{ width: "100%" }}>
+        <View style={styles.devices}>
+          <View style={styles.yellowBox}>
+            <View style={styles.livingRoom}>
+              <View style={styles.box}></View>
+              <Text>Heater</Text>
+            </View>
 
-    <Button title="On" onPress={() => setColorFunction3("green")}></Button>
-    <Button title="Off" onPress={() => setColorFunction3("red")}></Button>
+            <View>
+              <Button
+                title="On"
+                onPress={() => setColorFunction1("green")}
+              ></Button>
+              <Button
+                title="Off"
+                onPress={() => setColorFunction1("red")}
+              ></Button>
+            </View>
+          </View>
+        </View>
+      </View>
 
-    </View>
-    </View>
+      <View style={{ width: "100%" }}>
+        <View style={styles.devices}>
+          <View style={styles.yellowBox}>
+            <View style={styles.livingRoom}>
+              <View style={styles.box}></View>
+              <Text>TV</Text>
+            </View>
 
-    <View style = {{position: "absolute", display: "flex", flexDirection: "column", top: "59%", justifyContent: "flex-end"}}>  
-    <View style = {{margin: "20px", width: 20,height: 20,backgroundColor: boxColor1 }} /> 
-    <View style = {{margin: "20px", width: 20,height: 20,backgroundColor: boxColor2 }} />
-    <View style = {{margin: "20px", width: 20,height: 20,backgroundColor: boxColor3 }} />  
-    </View>
-    
-    <View>
+            <View>
+              <Button
+                title="On"
+                onPress={() => setColorFunction1("green")}
+              ></Button>
+              <Button
+                title="Off"
+                onPress={() => setColorFunction1("red")}
+              ></Button>
+            </View>
+          </View>
+        </View>
+      </View>
 
-    </View>
-
+      <Text>Devices on: </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -98,4 +139,62 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
 
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeigh: "bold,",
+  },
+
+  // Rooms
+  rooms: {
+    width: "100%",
+    alignItems: "center",
+  },
+
+  roomsContainer: {
+    width: "90%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  roomsbackground: {
+    backgroundColor: "purple",
+    padding: 12,
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+  },
+
+  // Devices
+  devices: {
+    alignItems: "center",
+    width: "100%",
+  },
+
+  yellowBox: {
+    width: "90%",
+    padding: 5,
+    marginTop: 10,
+    flexDirection: "row",
+    backgroundColor: "#F4FD89",
+  },
+
+  livingRoom: {
+    width: "87%",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  box: {
+    padding: 10,
+    marginHorizontal: 10,
+    backgroundColor: "red",
+  },
 });
